9c16467fd1f1820297729944c3f3e7357c6adde7
nitinsomu <nitin.somu13@gmail.com>
2024-11-17 15:13:18 +0530

    Add style

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from ultralytics import YOLO
 import os
+from PIL import Image
 import torch
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
 
-model = YOLO('../runs/classify/train/weights/best.pt') 
+# Load the YOLO model
+model = YOLO('../runs/classify/train/weights/best.pt')   # Replace with your YOLO model path
 
+# Check if the file extension is allowed
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
@@ -25,13 +29,17 @@ def predict():
         return "No selected file", 400
 
     if file and allowed_file(file.filename):
-
+        # Save the uploaded file
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        results = model(filepath)
+        file.save(filepath)
+
+        # Perform prediction
+        results = model.predict(source=filepath)
         prediction = results[0].names[torch.argmax(results[0].probs.data).item()] 
         return render_template('index.html', prediction=prediction, image_url=filepath)
 
     return "Invalid file type", 400
 
 if __name__ == '__main__':
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     app.run(debug=True)
